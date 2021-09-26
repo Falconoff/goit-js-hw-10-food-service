5e4cf8849f275bdc858009c817023dc7b32ba5c0
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-09-26 22:32:32 +0300

    fix double code and add Theme consts

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,23 +1,29 @@
 const themeSwitcher = document.getElementById('theme-switch-toggle');
 const bodyClass = document.querySelector('body').classList;
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 themeSwitcher.addEventListener('change', onChangeTheme);
 
 function onChangeTheme() {
   if (themeSwitcher.checked) {
-    console.log('TRUE - Night');
-    bodyClass.add('dark-theme');
-    bodyClass.remove('light-theme');
-    localStorage.setItem('theme', 'dark');
+    changeThemeClassAndSave({ add: Theme.DARK, del: Theme.LIGHT });
     return;
   }
-  console.log('FALSE - Day');
-  bodyClass.add('light-theme');
-  bodyClass.remove('dark-theme');
-  localStorage.setItem('theme', 'light');
+  changeThemeClassAndSave({ add: Theme.LIGHT, del: Theme.DARK });
 }
 
-if (localStorage.getItem('theme') === 'dark') {
+if (localStorage.getItem('theme') === 'dark-theme') {
   bodyClass.add('dark-theme');
   themeSwitcher.setAttribute('checked', 'checked');
+}
+
+function changeThemeClassAndSave(obj) {
+  const { add, del } = obj;
+
+  bodyClass.add(add);
+  bodyClass.remove(del);
+  localStorage.setItem('theme', add);
 }
