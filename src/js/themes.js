9d8716f82e8ff12659c08f64ff1bd98e7f55d9e1
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-09-26 22:33:52 +0300

    Update themes.js

--- src/js/themes.js
+++ src/js/themes.js
@@ -7,17 +7,17 @@ const Theme = {
 
 themeSwitcher.addEventListener('change', onChangeTheme);
 
+if (localStorage.getItem('theme') === 'dark-theme') {
+  bodyClass.add('dark-theme');
+  themeSwitcher.setAttribute('checked', 'checked');
+}
+
 function onChangeTheme() {
   if (themeSwitcher.checked) {
     changeThemeClassAndSave({ add: Theme.DARK, del: Theme.LIGHT });
     return;
   }
   changeThemeClassAndSave({ add: Theme.LIGHT, del: Theme.DARK });
-}
-
-if (localStorage.getItem('theme') === 'dark-theme') {
-  bodyClass.add('dark-theme');
-  themeSwitcher.setAttribute('checked', 'checked');
 }
 
 function changeThemeClassAndSave(obj) {
